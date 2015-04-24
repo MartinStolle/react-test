9e4cfb3bc29cd845c21628ae3e628763a6411e9e
Martin Stolle <m.stolle85@gmail.com>
2015-04-24 20:14:25 +0200

    Moving forward in the tutorial

--- src/helloworld.jsx
+++ src/helloworld.jsx
@@ -1,30 +1,65 @@
-// tutorial1.js
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.state.data}/>
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
 var CommentList = React.createClass({
-    render: function() {
-        return ( < div className = "commentList" >
-            Hello, world!I am a CommentList. < /div>
-        );
-    }
+  render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
 });
 
 var CommentForm = React.createClass({
-    render: function() {
-        return ( < div className = "commentForm" >
-            Hello, world!I am a CommentForm. < /div>
-        );
-    }
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
 });
 
-var CommentBox = React.createClass({
-    render: function() {
-        return ( < div className = "commentBox" >
-            < h1 > Comments < /h1> < CommentList / >
-            < CommentForm / >
-            < /div>
-        );
-    }
+var Comment = React.createClass({
+  render: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+      </div>
+    );
+  }
 });
 
-React.render( < CommentBox / > ,
-    document.getElementById('content')
+React.render(
+  <CommentBox data={data} />,
+  document.getElementById('content')
 );
